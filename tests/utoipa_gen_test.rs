4b7dfb34677c5fc2a78f9cf7f6febf1ef4fdca1f
Juha Kukkonen <juha7kukkonen@gmail.com>
2022-01-18 02:41:57 +0200

    Update README.md

--- tests/utoipa_gen_test.rs
+++ tests/utoipa_gen_test.rs
@@ -6,43 +6,43 @@ use serde_json::json;
 use utoipa::{Component, OpenApi};
 
 #[derive(Deserialize, Serialize, Component)]
-struct Foo {
-    ids: Vec<i32>,
+struct Pet {
+    id: u64,
+    name: String,
+    age: Option<i32>,
 }
 
-// mod api {
-//     use super::*;
+mod pet_api {
+    use super::*;
 
-/// Delete foo entity
-///
-/// Delete foo entity by what
-#[utoipa::path(
-    request_body = (content = Foo, required, description = "foobar", content_type = "text/xml"), 
-    responses = [
-        (status = 200, description = "success response", body = [Foo], headers = [("xrfs-token" = u64)])
-        // (400, "my bad error", u64),
-        // (404, "vault not found"),
-        // (500, "internal server error")
-    ],
-     params = [
-        ("ids" = [i32], query, description = "Search foos by ids"),
-   ]
-)]
-#[get("/foo/{_:.*}")]
-// #[deprecated = "this is deprecated"]
-// web::Path(id): web::Path<i32>
-async fn foo_delete(web::Query(foo): web::Query<Foo>) -> impl Responder {
-    let ids = foo.ids;
-    HttpResponse::Ok().json(json!({ "searched": ids }))
+    /// Get pet by id
+    ///
+    /// Get pet from database by pet database id  
+    #[utoipa::path(
+        get,
+        path = "/pets/{id}"
+        responses = [
+            (status = 200, description = "Pet found succesfully", body = Pet),
+            (status = 404, description = "Pet was not found")
+        ],
+        params = [
+            ("id" = u64, path, description = "Pet database id to get Per for"),
+        ]
+    )]
+    async fn get_pet_by_id(pet_id: u64) -> Pet {
+        Pet {
+            id: pet_id,
+            age: None,
+            name: "lightning".to_string(),
+        }
+    }
 }
-// }
 
 #[test]
 #[ignore = "this is just a test bed to run macros"]
 fn derive_openapi() {
-    // use crate::api::__path_foo_delete;
     #[derive(OpenApi, Default)]
-    #[openapi(handlers = [foo_delete], components = [Foo])]
+    #[openapi(handlers = [pet_api::get_pet_by_id], components = [Pet])]
     struct ApiDoc;
 
     println!("{}", ApiDoc::openapi().to_pretty_json().unwrap());
