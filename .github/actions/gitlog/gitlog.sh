5d58e089cf075d061a6a5bd4234e022113def374
Juha Kukkonen <juha7kukkonen@gmail.com>
2022-02-14 01:32:24 +0200

    Ci tweaking (#19)
    * Add draft a new release workflow -> Drafts a new release or updates existing one after push or merge to master
    * Add publish release workflow -> Will publish released crates to crates.io based on changed Cargo.toml files

---
+++ .github/actions/gitlog/gitlog.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# This mangles git log entries for change lop purposes
+
+from_commit=HEAD
+last_release=$(git tag --sort=-taggerdate | head -1) # get last tag
+
+output_file=""
+while true; do
+  case $1 in
+    "--output-file")
+      shift
+      output_file="$1"
+      shift
+    ;;
+    *)
+      break
+    ;;
+  esac
+done
+
+commit_range=""
+if [[ $last_release != "" ]]; then
+  commit_range="$from_commit...$last_release"
+  len=${#log_lines[@]}
+  unset "log_lines[(($len - 1))]" # remove the last item since it is the actual tag
+else
+  commit_range="$from_commit"
+fi
+
+ancestry_path=""
+if [[ "$last_release" != "" ]]; then
+  ancestry_path="--ancestry-path"
+fi
+
+mapfile -t log_lines < <(git log --pretty=format:'(%h) %s' $ancestry_path $commit_range)
+
+log=""
+for line in "${log_lines[@]}"; do
+  log=$log"* $line\n"
+done
+if [[ "$output_file" != "" ]]; then
+  echo -e "$log" > "$output_file"
+fi
