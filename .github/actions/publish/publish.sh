49a5220d6160331e15fc88f7e877111ef25e18a9
Juha Kukkonen <juha7kukkonen@gmail.com>
2022-02-14 23:23:04 +0200

    Extract inline publish step to separate action
    * Create seprate action for publish to crates.io making it simplier to maintain.
    * Now released crates are not based on lastest change but all will be released.

---
+++ .github/actions/publish/publish.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Publishes crate to crates.io
+
+token=""
+while true; do
+  case $1 in
+    "--token")
+      shift
+      token="$1"
+      shift
+    ;;
+    *)
+      break
+    ;;
+  esac
+done
+
+if [[ "$token" == "" ]]; then
+  echo "Missing --token <token> option argument, cannot publish crates without it!" && exit 1
+fi
+
+function publish {
+  module="$1"
+  if [[ "$module" == "utoipa" ]]; then
+    cargo publish
+  else
+    cargo publish -p "$module"
+  fi
+}
+
+echo "$token" | cargo login
+while read -r manifest; do
+  echo "Publishing module $manifest..."
+  module=$(cargo read-manifest --manifest-path "$manifest" | jq -r .name)
+  
+  max_retries=5
+  retry=0
+  while ! publish "$module" && [[ $retry -lt $max_retries ]]; do
+    await_time=$((retry*2))
+    echo "Failed to publish, Retrying $retry... after $await_time sec."
+    sleep $await_time
+    retry=$((retry+1))
+  done
+  if [[ $retry -eq $max_retries ]]; then
+    echo "Failed to publish crate $module, try to increase await time? Or retries?" && exit 1
+  fi
+done < <(find . ! -path "*target*" -name "Cargo.toml" | sort -r)
