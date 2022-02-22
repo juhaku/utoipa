516e09cce885f122fcc75fdfe3d516efc58ca281
Juha Kukkonen <juha7kukkonen@gmail.com>
2022-02-23 00:41:12 +0200

    Wip add security schema

--- src/openapi/security.rs
+++ src/openapi/security.rs
@@ -14,3 +14,346 @@ impl Security {
         Default::default()
     }
 }
+
+#[non_exhaustive]
+#[derive(Serialize, Deserialize, Clone)]
+#[cfg_attr(feature = "debug", derive(Debug))]
+#[serde(rename_all = "camelCase")]
+pub struct SecuritySchema {
+    #[serde(rename = "type")]
+    schema_type: String,
+
+    #[serde(skip_serializing_if = "String::is_empty")]
+    description: String,
+
+    #[serde(flatten)]
+    security_type: SecurityType,
+}
+
+impl SecuritySchema {
+    pub fn new(security_type: SecurityType) -> Self {
+        Self {
+            schema_type: String::from(SecuritySchema::resolve_type(&security_type)),
+            description: String::new(),
+            security_type,
+        }
+    }
+
+    fn resolve_type(security_type: &SecurityType) -> &str {
+        match security_type {
+            SecurityType::OAuth2 { .. } => "oauth2",
+            SecurityType::ApiKey { .. } => "apiKey",
+            SecurityType::Http { .. } => "http",
+            SecurityType::OpenIdConnect { .. } => "openIdConnect",
+        }
+    }
+
+    pub fn with_description<S: Into<String>>(mut self, description: S) -> Self {
+        self.description = description.into();
+
+        self
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(untagged)]
+#[cfg_attr(feature = "debug", derive(Debug))]
+pub enum SecurityType {
+    #[serde(rename_all = "camelCase")]
+    OAuth2 {
+        #[serde(flatten)]
+        flows: Flows,
+    },
+    #[serde(rename_all = "camelCase")]
+    ApiKey {
+        name: String,
+        #[serde(rename = "in")]
+        api_key_in: ApiKeyIn,
+    },
+    #[serde(rename_all = "camelCase")]
+    Http {
+        schema: HttpAutheticationType,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        bearer_format: Option<String>,
+    },
+    #[serde(rename_all = "camelCase")]
+    OpenIdConnect { open_id_connect_url: String },
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[cfg_attr(feature = "debug", derive(Debug))]
+pub struct Flows {
+    flows: HashMap<String, Flow>,
+}
+
+impl Flows {
+    pub fn new<I: IntoIterator<Item = Flow>>(flows: I) -> Self {
+        Self {
+            flows: HashMap::from_iter(
+                flows
+                    .into_iter()
+                    .map(|auth| (String::from(auth.get_type_as_str()), auth)),
+            ),
+        }
+    }
+}
+/// Implements types according [RFC7235](https://datatracker.ietf.org/doc/html/rfc7235#section-5.1) which are maintained in
+/// https://www.iana.org/assignments/http-authschemes/http-authschemes.xhtml
+#[derive(Serialize, Deserialize, Clone)]
+#[cfg_attr(feature = "debug", derive(Debug))]
+#[serde(rename_all = "lowercase")]
+pub enum HttpAutheticationType {
+    Basic,
+    Bearer,
+    Digest,
+    Hoba,
+    Mutual,
+    Negotiate,
+    OAuth,
+    #[serde(rename = "scram-sha-1")]
+    ScramSha1,
+    #[serde(rename = "scram-sha-256")]
+    ScramSha256,
+    Vapid,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(rename_all = "lowercase")]
+#[cfg_attr(feature = "debug", derive(Debug))]
+pub enum ApiKeyIn {
+    Header,
+    Query,
+    Cookie,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(untagged)]
+#[cfg_attr(feature = "debug", derive(Debug))]
+pub enum Flow {
+    Implicit(OAuth2Flow),
+    Password(OAuth2Flow),
+    ClientCredentials(OAuth2Flow),
+    AuthorizationCode(OAuth2Flow),
+}
+
+impl Flow {
+    fn get_type_as_str(&self) -> &str {
+        match self {
+            Self::Implicit(_) => "implicit",
+            Self::Password(_) => "password",
+            Self::ClientCredentials(_) => "clientCredentials",
+            Self::AuthorizationCode(_) => "authorizationCode",
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+#[cfg_attr(feature = "debug", derive(Debug))]
+pub struct OAuth2Flow {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    authorization_url: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    token_url: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    refresh_url: Option<String>,
+
+    scopes: HashMap<String, String>,
+}
+
+impl OAuth2Flow {
+    pub fn new<A: Into<String>, T: Into<String>, R: Into<String>>(
+        authorization_url: Option<A>,
+        token_url: Option<T>,
+        refresh_url: Option<R>,
+        scopes: HashMap<String, String>,
+    ) -> Self {
+        Self {
+            authorization_url: authorization_url.map(Into::into),
+            token_url: token_url.map(Into::into),
+            refresh_url: refresh_url.map(Into::into),
+            scopes,
+        }
+    }
+}
+
+#[cfg(test)]
+#[cfg(feature = "json")]
+mod tests {
+    use super::*;
+
+    macro_rules! test_fn {
+        ($name:ident: $schema:expr; $expected:literal) => {
+            #[test]
+            fn $name() {
+                let s = $schema;
+                let actual = serde_json::to_string_pretty(&s).unwrap();
+                assert_eq!(
+                    actual,
+                    $expected,
+                    "testing serializing \"{}\": \nactual:\n{}\nexpected:\n{}",
+                    stringify!($name),
+                    actual,
+                    $expected
+                );
+
+                println!("{}", &actual);
+            }
+        };
+    }
+
+    test_fn! {
+    security_schema_correct_http_bearer_json:
+    SecuritySchema::new(SecurityType::Http {
+                bearer_format: Some("JWT".to_string()),
+                schema: HttpAutheticationType::Bearer,
+            });
+    r###"{
+  "type": "http",
+  "schema": "bearer",
+  "bearerFormat": "JWT"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_basic_auth:
+        SecuritySchema::new(SecurityType::Http{schema: HttpAutheticationType::Basic, bearer_format: None});
+        r###"{
+  "type": "http",
+  "schema": "basic"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_digest_auth:
+        SecuritySchema::new(SecurityType::Http{schema: HttpAutheticationType::Digest, bearer_format: None});
+        r###"{
+  "type": "http",
+  "schema": "digets"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_hoba_auth:
+        SecuritySchema::new(SecurityType::Http{schema: HttpAutheticationType::Hoba, bearer_format: None});
+        r###"{
+  "type": "http",
+  "schema": "hoba"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_mutual_auth:
+        SecuritySchema::new(SecurityType::Http{schema: HttpAutheticationType::Mutual, bearer_format: None});
+        r###"{
+  "type": "http",
+  "schema": "mutual"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_negotiate_auth:
+        SecuritySchema::new(SecurityType::Http{schema: HttpAutheticationType::Negotiate, bearer_format: None});
+        r###"{
+  "type": "http",
+  "schema": "negotiate"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_oauth_auth:
+        SecuritySchema::new(SecurityType::Http{schema: HttpAutheticationType::OAuth, bearer_format: None});
+        r###"{
+  "type": "http",
+  "schema": "oauth"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_scram_sha1_auth:
+        SecuritySchema::new(SecurityType::Http{schema: HttpAutheticationType::ScramSha1, bearer_format: None});
+        r###"{
+  "type": "http",
+  "schema": "scram-sha-1"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_scram_sha256_auth:
+        SecuritySchema::new(SecurityType::Http{schema: HttpAutheticationType::ScramSha256, bearer_format: None});
+        r###"{
+  "type": "http",
+  "schema": "scram-sha-256"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_api_key_cookie_auth:
+        SecuritySchema::new(SecurityType::ApiKey{api_key_in: ApiKeyIn::Cookie , name: String::from("api_key")});
+        r###"{
+  "type": "apiKey",
+  "name": "api_key",
+  "in": "cookie"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_api_key_header_auth:
+        SecuritySchema::new(SecurityType::ApiKey{api_key_in: ApiKeyIn::Header , name: String::from("api_key")});
+        r###"{
+  "type": "apiKey",
+  "name": "api_key",
+  "in": "header"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_api_key_query_auth:
+        SecuritySchema::new(SecurityType::ApiKey{api_key_in: ApiKeyIn::Query , name: String::from("api_key")});
+        r###"{
+  "type": "apiKey",
+  "name": "api_key",
+  "in": "query"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_open_id_connect_auth:
+        SecuritySchema::new(SecurityType::OpenIdConnect{open_id_connect_url: String::from("http://localhost/openid")});
+        r###"{
+  "type": "openIdConnect",
+  "openIdConnectUrl": "http://localhost/openid"
+}"###
+    }
+
+    test_fn! {
+        security_schema_correct_oauth2_implicit:
+        SecuritySchema::new(SecurityType::OAuth2 {
+            flows: Flows::new([Flow::Implicit(
+        OAuth2Flow::new(
+            Some("http://localhost/auth/dialog"),
+            None::<&str>,
+            None::<&str>,
+            HashMap::from([
+                ("edit:items".to_string(), "edit my items".to_string()),
+                ("read:items".to_string(), "read my items".to_string()
+            )]),
+        ),
+    )])
+        });
+        r###"{
+  "type": "oauth2",
+  "flows": {
+    "implicit": {
+      "authorizationUrl": "http://localhost/auth/dialog",
+      "scopes": {
+        "edit:items": "edit my items",
+        "read:items": "read my items"
+      }
+    }
+  }
+}"###
+    }
+}
