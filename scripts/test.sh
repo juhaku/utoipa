273af787941055ee9bbd5b546fedff6f708451a4
Juha Kukkonen <juha7kukkonen@gmail.com>
2022-04-25 19:33:08 +0300

    Add scripts

---
+++ scripts/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+crate="$1"
+
+echo "Testing crate: $crate..."
+
+if [[ "$crate" == "utoipa" ]]; then
+  cargo test
+  cargo test --test path_response_derive_test_no_serde_json --no-default-features
+  cargo test --test component_derive_no_serde_json --no-default-features
+  cargo test --test path_derive_actix --test path_parameter_derive_actix --features actix_extras
+  cargo test --test component_derive_test --features chrono_types,decimal
+  cargo test --test component_derive_test --features chrono_types_with_format
+  cargo test --test path_derive_rocket --features rocket_extras,json
+  elif [[ "$crate" == "utoipa-gen" ]]; then
+  cargo test -p utoipa-gen --features utoipa/actix_extras
+  elif [[ "$crate" == "utoipa-swagger-ui" ]]; then
+  cargo test -p utoipa-swagger-ui --features actix-web,rocket
+fi
