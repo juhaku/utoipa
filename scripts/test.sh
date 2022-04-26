9a6d198bb56700b4090477ad8cdd753ee6f79a4f
Juha Kukkonen <juha7kukkonen@gmail.com>
2022-04-26 17:12:42 +0300

    Update test features

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,11 +5,11 @@ crate="$1"
 echo "Testing crate: $crate..."
 
 if [[ "$crate" == "utoipa" ]]; then
-  cargo test
+  cargo test --features uuid
   cargo test --test path_response_derive_test_no_serde_json --no-default-features
   cargo test --test component_derive_no_serde_json --no-default-features
   cargo test --test path_derive_actix --test path_parameter_derive_actix --features actix_extras
-  cargo test --test component_derive_test --features chrono_types,decimal
+  cargo test --test component_derive_test --features chrono_types,decimal,uuid
   cargo test --test component_derive_test --features chrono_types_with_format
   cargo test --test path_derive_rocket --features rocket_extras,json
   elif [[ "$crate" == "utoipa-gen" ]]; then
