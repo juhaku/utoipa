4b22f94fefcd311d5b220591fc25bd9245263d2b
Juha Kukkonen <juha7kukkonen@gmail.com>
2022-07-12 00:11:51 +0300

    Add path support for `IntoParams`
    Allow comparing types by their `path` for `IntoParams` types. Previously
    comparison was done based on `Ident` causing name clashes in case there
    would be `IntoParams` type with same name but different module.

--- utoipa-gen/src/path/parameter.rs
+++ utoipa-gen/src/path/parameter.rs
@@ -1,4 +1,4 @@
-use std::{borrow::Cow, fmt::Display, mem};
+use std::{borrow::Cow, fmt::Display};
 
 use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, quote, quote_spanned, ToTokens};
