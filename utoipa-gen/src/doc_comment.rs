0ba90ecec685e502043d8886ad6b82b86e084b35
trinity-1686a <trinity@deuxfleurs.fr>
2023-07-25 19:12:55 +0200

    allow and ignore #[doc(...)] tags in ToShema derive

--- utoipa-gen/src/doc_comment.rs
+++ utoipa-gen/src/doc_comment.rs
@@ -1,7 +1,6 @@
 use std::ops::Deref;
 
 use proc_macro2::Ident;
-use proc_macro_error::abort_call_site;
 use syn::{Attribute, Expr, Lit, Meta};
 
 const DOC_ATTRIBUTE_TYPE: &str = "doc";
@@ -50,7 +49,8 @@ impl CommentAttributes {
                     None
                 }
             }
-            _ => abort_call_site!("Expected only Meta::NameValue type"),
+            // ignore `#[doc(hidden)]` and similar tags.
+            _ => None,
         }
     }
 
