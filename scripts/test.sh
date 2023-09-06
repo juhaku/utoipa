ff77bb4d0620790d8e0fd866fdc56e02f1746b35
John Vandenberg <jayvdb@gmail.com>
2023-09-06 18:01:15 +0800

    Allow additionalProperties to be an array

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,28 +2,32 @@
 
 set -e
 
-crate="$1"
+: "${CARGO:=cargo}"
 
-echo "Testing crate: $crate..."
+crates="${1:-utoipa utoipa-gen utoipa-swagger-ui utoipa-redoc utoipa-rapidoc}"
 
-if [[ "$crate" == "utoipa" ]]; then
-    cargo test -p utoipa --features openapi_extensions,preserve_order,preserve_path_order,debug
-elif [[ "$crate" == "utoipa-gen" ]]; then
-    cargo test -p utoipa-gen --features utoipa/actix_extras,chrono,decimal,utoipa/uuid,uuid,utoipa/ulid,ulid,utoipa/url,url,utoipa/time,time,utoipa/repr,utoipa/smallvec,smallvec,rc_schema,utoipa/rc_schema
+for crate in $crates; do
+    echo "Testing crate: $crate..."
 
-    cargo test -p utoipa-gen --test schema_derive_test --features decimal_float
-    cargo test -p utoipa-gen --test path_derive_auto_into_responses --features auto_into_responses,utoipa/uuid,uuid
-    cargo test -p utoipa-gen --test path_derive_actix --test path_parameter_derive_actix --features actix_extras,utoipa/uuid,uuid
-    cargo test -p utoipa-gen --test path_derive_auto_into_responses_actix --features actix_extras,utoipa/auto_into_responses,utoipa/uuid,uuid
+    if [[ "$crate" == "utoipa" ]]; then
+        $CARGO test -p utoipa --features openapi_extensions,preserve_order,preserve_path_order,debug
+    elif [[ "$crate" == "utoipa-gen" ]]; then
+        $CARGO test -p utoipa-gen --features utoipa/actix_extras,chrono,decimal,utoipa/uuid,uuid,utoipa/ulid,ulid,utoipa/url,url,utoipa/time,time,utoipa/repr,utoipa/smallvec,smallvec,rc_schema,utoipa/rc_schema
 
-    cargo test -p utoipa-gen --test path_derive_rocket --features rocket_extras
+        $CARGO test -p utoipa-gen --test schema_derive_test --features decimal_float
+        $CARGO test -p utoipa-gen --test path_derive_auto_into_responses --features auto_into_responses,utoipa/uuid,uuid
+        $CARGO test -p utoipa-gen --test path_derive_actix --test path_parameter_derive_actix --features actix_extras,utoipa/uuid,uuid
+        $CARGO test -p utoipa-gen --test path_derive_auto_into_responses_actix --features actix_extras,utoipa/auto_into_responses,utoipa/uuid,uuid
 
-    cargo test -p utoipa-gen --test path_derive_axum_test --features axum_extras
-    cargo test -p utoipa-gen --test path_derive_auto_into_responses_axum --features axum_extras,utoipa/auto_into_responses
-elif [[ "$crate" == "utoipa-swagger-ui" ]]; then
-    cargo test -p utoipa-swagger-ui --features actix-web,rocket,axum
-elif [[ "$crate" == "utoipa-redoc" ]]; then
-    cargo test -p utoipa-redoc --features actix-web,rocket,axum
-elif [[ "$crate" == "utoipa-rapidoc" ]]; then
-    cargo test -p utoipa-rapidoc --features actix-web,rocket,axum
-fi
+        $CARGO test -p utoipa-gen --test path_derive_rocket --features rocket_extras
+
+        $CARGO test -p utoipa-gen --test path_derive_axum_test --features axum_extras
+        $CARGO test -p utoipa-gen --test path_derive_auto_into_responses_axum --features axum_extras,utoipa/auto_into_responses
+    elif [[ "$crate" == "utoipa-swagger-ui" ]]; then
+        $CARGO test -p utoipa-swagger-ui --features actix-web,rocket,axum
+    elif [[ "$crate" == "utoipa-redoc" ]]; then
+        $CARGO test -p utoipa-redoc --features actix-web,rocket,axum
+    elif [[ "$crate" == "utoipa-rapidoc" ]]; then
+        $CARGO test -p utoipa-rapidoc --features actix-web,rocket,axum
+    fi
+done
