2c86a4d34be811c489cb0f37f09506be47905940
Tim DuBois <tim@neophilus.net>
2023-11-27 10:42:41 +0100

    Update Redoc endpoint to axum 0.7

--- utoipa-redoc/src/axum.rs
+++ utoipa-redoc/src/axum.rs
@@ -1,19 +1,17 @@
 #![cfg(feature = "axum")]
 
-use axum::body::HttpBody;
 use axum::response::Html;
 use axum::{routing, Router};
 
 use crate::{Redoc, Spec};
 
-impl<'s, 'u, S: Spec, R, B> From<Redoc<'s, 'u, S>> for Router<R, B>
+impl<'s, 'u, S: Spec, R> From<Redoc<'s, 'u, S>> for Router<R>
 where
     R: Clone + Send + Sync + 'static,
-    B: HttpBody + Send + 'static,
     's: 'static,
 {
     fn from(value: Redoc<'s, 'u, S>) -> Self {
         let html = value.to_html();
-        Router::<R, B>::new().route(value.url, routing::get(move || async { Html(html) }))
+        Router::<R>::new().route(value.url, routing::get(move || async { Html(html) }))
     }
 }
