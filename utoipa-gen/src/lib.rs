c618dbe42fe5c4889b7925f2584a5e56cb48cccb
cuiihaoo <107397001+cuiihaoo@users.noreply.github.com>
2024-05-15 13:58:46 +0800

    change pub(super) enum to pub enum (#926)
    Co-authored-by: cuihao <cuiiihaoo@gmail.com>

--- utoipa-gen/src/lib.rs
+++ utoipa-gen/src/lib.rs
@@ -2918,7 +2918,7 @@ mod parse_utils {
 
     #[derive(Clone)]
     #[cfg_attr(feature = "debug", derive(Debug))]
-    pub(super) enum Str {
+    pub enum Str {
         String(String),
         IncludeStr(TokenStream),
     }
