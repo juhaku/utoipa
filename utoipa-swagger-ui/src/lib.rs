de91d645d3f58f9e5acd74c689d56915ffe93b28
Juha Kukkonen <juha7kukkonen@gmail.com>
2024-12-09 17:16:51 +0200

    Update utoipa-swagger-ui/src/lib.rs

--- utoipa-swagger-ui/src/lib.rs
+++ utoipa-swagger-ui/src/lib.rs
@@ -1341,6 +1341,8 @@ impl From<String> for Config<'_> {
     }
 }
 
+/// Basic auth options for Swagger UI. By providing `BasicAuth` to `Config::basic_auth` the access to the
+/// Swagger UI can be restricted behind given basic authentication. 
 #[derive(Serialize, Clone)]
 pub struct BasicAuth {
     pub username: String,
