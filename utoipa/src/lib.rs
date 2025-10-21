3514a71aa23609442db27ef07c3410ec8cd14219
hdbg <httpdebugger@protonmail.com>
2025-10-21 10:47:46 +0200

    fix(bytes): forgot to add `ToSchema` impl

--- utoipa/src/lib.rs
+++ utoipa/src/lib.rs
@@ -800,11 +800,14 @@ impl PartialSchema for serde_json::Value {
 impl ToSchema for serde_json::Value {}
 
 #[cfg(feature = "bytes")]
-impl PartialSchema for bytes::Bytes {
-    fn schema() -> openapi::RefOr<openapi::schema::Schema> {
-        <Vec<u8> as PartialSchema>::schema()
+const _: () = {
+    impl PartialSchema for bytes::Bytes {
+        fn schema() -> openapi::RefOr<openapi::schema::Schema> {
+            <Vec<u8> as PartialSchema>::schema()
+        }
     }
-}
+    impl ToSchema for bytes::Bytes {}
+};
 
 // Create `utoipa` module so we can use `utoipa-gen` directly from `utoipa` crate.
 // ONLY for internal use!
