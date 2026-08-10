6ea74d60664ac63a27c7ae2baf77fbf6676652bd
Jaye <latenytcoffee@gmail.com>
2026-08-10 17:49:23 +0100

    chore: use openapi version attribute
    As https://github.com/juhaku/utoipa/pull/1591/ is merged, I can remove `api.openapi = utoipa::openapi::OpenApiVersion::Version32;` from the example to replace with `#[openapi(version = "3.2.0", components(schemas(PetEvent)))]`.

--- examples/openapi-3_2-sse/src/main.rs
+++ examples/openapi-3_2-sse/src/main.rs
@@ -72,17 +72,14 @@ async fn pet_events() -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
     Sse::new(stream).keep_alive(KeepAlive::new().interval(Duration::from_secs(15)))
 }
 
-/// Opt the whole document in to OpenAPI 3.2 output via `ApiDoc::openapi().openapi(...)`
+/// Opt the whole document in to OpenAPI 3.2 output with `version = "3.2.0"`
 #[derive(OpenApi)]
-#[openapi(components(schemas(PetEvent)))]
+#[openapi(version = "3.2.0", components(schemas(PetEvent)))]
 struct ApiDoc;
 
 #[tokio::main]
 async fn main() {
-    let mut api = ApiDoc::openapi();
-    api.openapi = utoipa::openapi::OpenApiVersion::Version32;
-
-    let (router, api) = OpenApiRouter::with_openapi(api)
+    let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
         .routes(routes!(pet_events))
         .split_for_parts();
 
@@ -102,10 +99,7 @@ mod tests {
 
     #[test]
     fn sse_response_documents_item_schema() {
-        let mut api = ApiDoc::openapi();
-        api.openapi = utoipa::openapi::OpenApiVersion::Version32;
-
-        let (_, api) = OpenApiRouter::<()>::with_openapi(api)
+        let (_, api) = OpenApiRouter::<()>::with_openapi(ApiDoc::openapi())
             .routes(routes!(pet_events))
             .split_for_parts();
 
